21a291d3da8f53b2bccdf350e2877d3218257e58
Tristan Luong <tristan.luong@gmail.com>
2019-07-14 16:16:33 +0200

    s3 upload

--- routes/images.js
+++ routes/images.js
@@ -8,19 +8,6 @@ const s3 = new AWS.S3({
 /* GET users listing. */
 router.get('/:image', async (req, res) => {
 
-  const response = await s3.listObjectsV2({
-    Bucket: 'bloomapi'
-  }).promise();
-
-  console.log(response);
-
-  var params = { Bucket: 'bloomapi', Key: req.params.image };
-  console.log(req.params.image)
-  s3.getObject(params, function(err, data) {
-    res.writeHead(200, {'Content-Type': 'image/jpg'});
-    res.write(data.Body, 'binary');
-    res.end(null, 'binary');
-  });
 });
 
 module.exports = router;
