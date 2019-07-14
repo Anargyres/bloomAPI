a114795d8396678950d6da04d7be8e513d9d2188
Tristan Luong <tristan.luong@gmail.com>
2019-07-14 15:14:18 +0200

    s3 upload

--- routes/images.js
+++ routes/images.js
@@ -6,16 +6,29 @@ const s3 = new AWS.S3({
   secretAccessKey: 'r6PcXbRLrWZolF5LAT0MPI2lVZHjAFf+X2+SkFKI'
 });
 /* GET users listing. */
-router.get('/:image', (req, res) => {
+router.get('/:image', async (req, res) => {
 
-  var params = { Bucket: 'bloomapi', Key: req.params.image };
-  console.log(req.params.image)
-  s3.getObject(params, function(err, data) {
-    console.log(data);
-    res.writeHead(200, {'Content-Type': 'image/jpg'});
-    res.write(data.Body, 'binary');
-    res.end(null, 'binary');
-  });
+  async function getObject (bucket, objectKey) {
+    try {
+      const params = {
+        Bucket: 'bloomapi',
+        Key: req.params.image
+      }
+
+      const data = await s3.getObject(params).promise();
+
+      return data.Body.toString('utf-8');
+    } catch (e) {
+      throw new Error(`Could not retrieve file from S3: ${e.message}`)
+    }
+  }
+
+  const image = await getObject();
+  res.writeHead(200, {'Content-Type': 'image/jpg'});
+  res.write(image, 'binary');
+  res.end(null, 'binary');
+
+
 });
 
 module.exports = router;
