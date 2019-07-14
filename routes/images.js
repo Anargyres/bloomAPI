bbe9b6030c5695ff3a616279ab588e38a543538a
Tristan Luong <tristan.luong@gmail.com>
2019-07-14 16:29:48 +0200

    s3 upload

--- routes/images.js
+++ routes/images.js
@@ -5,6 +5,17 @@ const AWS = require('aws-sdk');
 /* GET users listing. */
 router.get('/:image', async (req, res) => {
 
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  });
+
+  const params = { Bucket: 'bloomapi', Key: req.params.image };
+  s3.getObject(params, function(err, data) {
+    res.writeHead(200, {'Content-Type': 'image/jpg'});
+    res.write(data.Body, 'binary');
+    res.end(null, 'binary');
+  });
 });
 
 module.exports = router;
