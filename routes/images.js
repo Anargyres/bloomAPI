c91587c0911070b4aac98397cc77e7c3423872f9
Tristan Luong <tristan.luong@gmail.com>
2019-07-14 16:25:11 +0200

    s3 upload

--- routes/images.js
+++ routes/images.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3({
-  accessKeyId: 'AKIAJY3GD3CC2YLIGHUQ',
-  secretAccessKey: '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'
-});
+
 /* GET users listing. */
 router.get('/:image', async (req, res) => {
 
